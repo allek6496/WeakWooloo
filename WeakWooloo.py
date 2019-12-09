979f7aaab005f290da0426c2ef7f1c9a84be2576
WaleedAAA <58567993+WaleedAAA@users.noreply.github.com>
2019-12-09 12:41:21 -0500

    Added outline

--- WeakWooloo.py
+++ WeakWooloo.py
@@ -1,1 +1,19 @@
 '''bulletproof'''
+get move function
+
+'''
+have every variable
+1 is throw
+2 is duck
+3 is reload
+
+tree = [big array]
+
+sect = tree[round#] 
+
+look through everything in sect, looking for [yourSnow, oppSnow, yourDucks, oppDucks, yourPoints, oppPoints, 2*round#]
+
+when you find it, take the number after that list 
+
+[[yourSnow, oppSnow, yourDucks, oppDucks, yourPoints, oppPoints, 2*round#], moveToMake]
+'''
